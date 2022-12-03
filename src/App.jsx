3b4c34986a9f8ee29a0b725536562557e39e58ff
elios11 <eliosferre3@hotmail.com>
2022-12-03 20:28:33 -0300

    Change App to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
 import Card from "./components/Card/Card"
+import katie_img from "./assets/katie-zaferes-img.png"
 
 export default function App() {
     return (
@@ -8,7 +9,14 @@ export default function App() {
             <Navbar />
             <Hero />
             <div className="cards">
-                <Card />
+                <Card
+                    img={katie_img}
+                    rating={5.0}
+                    reviewCount={6}
+                    country="USA"
+                    title="Life lessons with Katie Zaferes"
+                    price={136}
+                />
             </div>
         </div>
     )
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,5 @@
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
-function Page() {
-  return (
-    <App />
-  )
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Page />)
+root.render(<App />)
