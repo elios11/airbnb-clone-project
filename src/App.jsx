0cdf4957d40d0af48e89cadb30fc2a677a9dd84e
elios11 <eliosferre3@hotmail.com>
2022-12-03 21:23:33 -0300

    Add card properties from mapped data

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
 import Card from "./components/Card/Card"
+import experiencesData from "./data"
 
 export default function App() {
+    const experiencesCards = experiencesData.map(experience => {
+        return <Card
+                    key={experience.id}
+                    img={`images/${experience.coverImg}`}
+                    rating={experience.stats.rating}
+                    reviewCount={experience.stats.reviewCount}
+                    location={experience.location}
+                    title={experience.title}
+                    price={experience.price}
+                    openSpots={experience.openSpots}
+               />
+    })
+
     return (
         <div>
             <Navbar />
             <Hero />
             <div className="cards">
-                <Card
-                    img="/images/katie-zaferes-img.png"
-                    rating={5.0}
-                    reviewCount={6}
-                    country="USA"
-                    title="Life lessons with Katie Zaferes"
-                    price={136}
-                />
+                {experiencesCards}
+                {experiencesCards}
             </div>
         </div>
     )
